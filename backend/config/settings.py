68c5b43fa635e476bd85a4045621f9cc411787d1
dennis <machariad196@gmail.com>
2026-07-14 01:28:22 +0300

    update installed app location

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -34,10 +34,10 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "rest_framework",
     "health",
-    "organizations",
-    "assets",
-    "scanning",
-    "findings",
+    "apps.organizations",
+    "apps.assets",
+    "apps.scanning",
+    "apps.findings",
 ]
 
 MIDDLEWARE = [
