acc2a92233f3f5e17a9cbc894df9f7bd285885e0
dennis <machariad196@gmail.com>
2026-07-14 16:47:35 +0300

    serializer for organization module

---
+++ backend/apps/organizations/serializers.py
@@ -0,0 +1,10 @@
+from rest_framework import serializers
+
+from .models import Organization
+
+
+class OrganizationSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Organization
+        fields = ["id", "name", "root_domain", "is_active", "created_at", "updated_at"]
+        read_only_fields = ["id", "created_at", "updated_at"]
