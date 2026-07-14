a3aadafe75d0f27c8e61820fe9171567792f94e7
dennis <machariad196@gmail.com>
2026-07-15 00:30:39 +0300

    added django spectacular in installed apps

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -34,6 +34,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "rest_framework",
     "django_filters",
+    "drf_spectacular",
     "django_celery_beat",
     "health",
     "apps.organizations",  
