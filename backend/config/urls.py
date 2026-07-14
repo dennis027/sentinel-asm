e27f41b78176264829544609a53bc2801eccbb6b
dennis <machariad196@gmail.com>
2026-07-14 16:50:23 +0300

    added api url root

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -4,4 +4,6 @@ from django.urls import path, include
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("health/", include("health.urls")),
+    path("api/", include("apps.api.urls")),
+
 ]
