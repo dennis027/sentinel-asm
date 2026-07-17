30ead65723c93cd88c69e90c73288cc4177b6985
dennis <machariad196@gmail.com>
2026-07-17 16:24:50 +0300

    added new url for api to use in postman

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -1,14 +1,16 @@
 from django.contrib import admin
 from django.urls import path, include
 from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView, SpectacularSwaggerView
-
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView, TokenVerifyView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("health/", include("health.urls")),
     path("api/", include("apps.api.urls")),
+    path("api/token/", TokenObtainPairView.as_view(), name="token-obtain-pair"),
+    path("api/token/refresh/", TokenRefreshView.as_view(), name="token-refresh"),
+    path("api/token/verify/", TokenVerifyView.as_view(), name="token-verify"),
     path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
     path("api/docs/", SpectacularSwaggerView.as_view(url_name="schema"), name="swagger-ui"),
     path("api/redoc/", SpectacularRedocView.as_view(url_name="schema"), name="redoc"),
-
 ]
