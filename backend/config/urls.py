1581dd244c60e7bd2f9c22221d1c334df48f0d2e
dennis <machariad196@gmail.com>
2026-07-18 12:43:41 +0300

    adding media url for my static files

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf import settings
 from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView, SpectacularSwaggerView
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView, TokenVerifyView
 
@@ -13,4 +15,4 @@ urlpatterns = [
     path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
     path("api/docs/", SpectacularSwaggerView.as_view(url_name="schema"), name="swagger-ui"),
     path("api/redoc/", SpectacularRedocView.as_view(url_name="schema"), name="redoc"),
-]
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
