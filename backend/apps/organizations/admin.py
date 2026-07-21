a2497b032696def7025a9537b31636d982c97c76
dennis <machariad196@gmail.com>
2026-07-21 23:18:14 +0300

    importing membership model in admins file

--- backend/apps/organizations/admin.py
+++ backend/apps/organizations/admin.py
@@ -2,11 +2,17 @@ from django.contrib import admin
 from .models import Membership, Organization
 
 
+class MembershipInline(admin.TabularInline):
+    model = Membership
+    extra = 0
+
+
 @admin.register(Organization)
 class OrganizationAdmin(admin.ModelAdmin):
     list_display = ("name", "root_domain", "is_active", "created_at")
     search_fields = ("name", "root_domain")
     list_filter = ("is_active",)
+    inlines = [MembershipInline]
 
 
 @admin.register(Membership)
