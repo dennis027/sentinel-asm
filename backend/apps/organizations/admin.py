e82e4c6ab9a5c658b00197e9f11a9f3f7ca3876d
dennis <machariad196@gmail.com>
2026-07-21 23:17:55 +0300

    importing membership model in admins file

--- backend/apps/organizations/admin.py
+++ backend/apps/organizations/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Organization
+from .models import Membership, Organization
 
 
 @admin.register(Organization)
@@ -7,3 +7,10 @@ class OrganizationAdmin(admin.ModelAdmin):
     list_display = ("name", "root_domain", "is_active", "created_at")
     search_fields = ("name", "root_domain")
     list_filter = ("is_active",)
+
+
+@admin.register(Membership)
+class MembershipAdmin(admin.ModelAdmin):
+    list_display = ("user", "organization", "role", "created_at")
+    list_filter = ("role", "organization")
+    search_fields = ("user__username", "organization__name")
